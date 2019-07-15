5d06d76f459989f37ace43d487783b4ac052d05d
Leon Kacowicz <leon.kacowicz@gmail.com>
2019-07-14 20:40:43 -0700

    Adding first arbiter test

--- test/arbiter/arbiter_test.cpp
+++ test/arbiter/arbiter_test.cpp
@@ -1,11 +1,31 @@
 #include <string>
 #include <gtest/gtest.h>
-#include <spawn.h>
 #include <process.h>
 #include <arbiter.h>
+#include <thread>
+#include <mutex>
+#include <deque>
 
 using namespace std;
 
-TEST(arbiter_test, arbiter_can_start_white_player) {
+TEST(arbiter_test, arbiter_can_start_players) {
 
+    stringstream white_in, white_out("uciok\n"), black_in, black_out("uciok\n");
+    player white(white_in, white_out);
+    player black(black_in, black_out);
+
+    arbiter arb(white, black, 10000, 0);
+
+    auto arb_th = thread([&] {
+        arb.start_players();
+    });
+    arb_th.detach();
+
+    this_thread::sleep_for(std::chrono::milliseconds(1));
+    string line;
+    getline(white_in, line);
+    ASSERT_STREQ(line.c_str(), "uci");
+
+    getline(black_in, line);
+    ASSERT_STREQ(line.c_str(), "uci");
 }
