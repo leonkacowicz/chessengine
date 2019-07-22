5f1d731efdcd8a0c27d5dab7f73d557b5e55a9ed
Leon Kacowicz <leon.kacowicz@gmail.com>
2019-07-21 20:36:51 -0700

    adding sanity tests to square_test

--- test/chess/square_test.cpp
+++ test/chess/square_test.cpp
@@ -1,5 +1,13 @@
 #include <gtest/gtest.h>
+#include <square.h>
 
 TEST(square_test, sanity_check_null_square_is_not_equal_to_any_other) {
-
+    square none;
+    for (char file = 'a'; file <= 'h'; file++) {
+        for (char rank = '1'; rank <= '8'; rank++) {
+            square sq(std::string({file, rank}));
+            std::cout << "Testing " << sq.to_string() << std::endl;
+            ASSERT_FALSE(sq == none);
+        }
+    }
 }
