80e1d72b27acfbfe49601eb2571878e52eebcb80
Leon Kacowicz <leon.kacowicz@gmail.com>
2019-07-29 21:19:41 -0700

    FEN string input

--- test/chess/board_test.cpp
+++ test/chess/board_test.cpp
@@ -21,12 +21,16 @@ TEST(board_test, fen_test) {
     board b;
     b.set_initial_position();
     ASSERT_EQ(b.fen(), "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    ASSERT_EQ(b, board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
     b.make_move(move("e2", "e4"));
     ASSERT_EQ(b.fen(), "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1");
+    ASSERT_EQ(b, board("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"));
     b.make_move(move("c7", "c5"));
     ASSERT_EQ(b.fen(2), "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2");
+    ASSERT_EQ(b, board("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"));
     b.make_move(move("g1", "f3"));
     ASSERT_EQ(b.fen(2), "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2");
+    ASSERT_EQ(b, board("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"));
 }
 
 TEST(legal_moves_king, king_first_rank) {
@@ -519,7 +523,25 @@ TEST(board_test, board_accepts_move) {
     // should be legal for white to move g4e5
 }
 
-// play 51 should not be legal: 1. Nf3  d5 2. Na3  Be6 3. h4  Qd7 4. b4  Bf5 5. Ng1  Qc6 6. c3  Qd7 7. Nb1  g5 8. c4  Kd8 9. Qa4  Qc8 10. Qb3  Qd7 11. Qh3  Qe8 12. g3  Bg7 13. cxd5  g4 14. Qxg4  c6 15. Qd4  Bh3 16. Qe4  Bh6 17. Qc2  Bf5 18. Bg2  Bg7 19. Qe4  Bc8 20. Ba3  Kd7 21. Qd3  Kc7 22. Qb5  Bg4 23. Qxb7+  Kd6 24. dxc6  h5 25. Qxa8  Qd8 26. Nf3  Qf8 27. Rg1  Qd8 28. Bh3  Bf6 29. Qb7  Bc8 30. Bf5  Nh6 31. Bd3  Bg5 32. Rg2  Ke6 33. c7  Kd7 34. Qa6  Bf4 35. Qc6+  Nxc6 36. Nd4  Bb7 37. Ba6  Bxg3 38. Nc2  Bc8 39. cxd8=B  Bb8 40. Ba5  Rd8 41. Rg6  Bf4 42. d4  f6 43. Bxd8  Kd6 44. Bb2  Ng8 45. Bxe7+  Nc6xe7 46. Ne3  Kc6 47. Ng2  Nf5 48. d5+  Kb6 49. Bc4  Bb8 50. f4  Bd7 51. O-O
+TEST(board_test, 1b4n1_p2b4_1k3pR1_3P1n1p_1PB2P1P_8_PB2P1N1_RN2K3__w__Q_____1__51) {
+
+    // play 51 should not be legal:
+    // 1. Nf3  d5 2. Na3  Be6 3. h4  Qd7 4. b4  Bf5 5. Ng1  Qc6 6. c3  Qd7 7. Nb1  g5
+    // 8. c4  Kd8 9. Qa4  Qc8 10. Qb3  Qd7 11. Qh3  Qe8 12. g3  Bg7 13. cxd5  g4 14. Qxg4  c6 15. Qd4  Bh3 16. Qe4  Bh6
+    // 17. Qc2  Bf5 18. Bg2  Bg7 19. Qe4  Bc8 20. Ba3  Kd7 21. Qd3  Kc7 22. Qb5  Bg4 23. Qxb7+  Kd6 24. dxc6  h5
+    // 25. Qxa8  Qd8 26. Nf3  Qf8 27. Rg1  Qd8 28. Bh3  Bf6 29. Qb7  Bc8 30. Bf5  Nh6 31. Bd3  Bg5 32. Rg2  Ke6
+    // 33. c7  Kd7 34. Qa6  Bf4 35. Qc6+  Nxc6 36. Nd4  Bb7 37. Ba6  Bxg3 38. Nc2  Bc8 39. cxd8=B  Bb8 40. Ba5  Rd8
+    // 41. Rg6  Bf4 42. d4  f6 43. Bxd8  Kd6 44. Bb2  Ng8 45. Bxe7+  Nc6xe7 46. Ne3  Kc6 47. Ng2  Nf5 48. d5+  Kb6
+    // 49. Bc4  Bb8 50. f4  Bd7 51. O-O
+
+    board b("1b4n1/p2b4/1k3pR1/3P1n1p/1PB2P1P/8/PB2P1N1/RN2K3 w Q - 1 51");
+
+    auto moves = b.get_legal_moves(WHITE);
+    for (auto& m : moves) {
+        ASSERT_NE(m.special, CASTLE_KING_SIDE_WHITE);
+    }
+}
+
 
 
 TEST(board_test, bug_detector) {
@@ -539,16 +561,14 @@ TEST(board_test, bug_detector) {
             k++;
             try {
                 unsigned long i = dis(gen) % legal_moves.size();
-//                auto castle = std::find_if(begin(legal_moves), end(legal_moves),
-//                                           [&](const move &other) { return other.special == CASTLE_QUEEN_SIDE_BLACK; });
-
-                //move m = (castle == end(legal_moves) ? legal_moves[i] : *castle);
                 move m = legal_moves[i];
                 pgn.push_back(b.move_in_pgn(m, legal_moves));
                 b.make_move(m);
                 if (b.under_check(opposite(b.side_to_play))) {
                     std::__throw_runtime_error("Move ignored check");
                 }
+                if (!(b == board(b.fen()))) std::__throw_runtime_error("FEN failed");
+
                 legal_moves = b.get_legal_moves(b.side_to_play);
             } catch (std::exception& e) {
                 std::cout << "Exception: " << e.what() << std::endl;
